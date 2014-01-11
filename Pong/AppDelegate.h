b11a69d757b78e114871c31e948b6f91a1d1bba8
Pablo Cambus <pablo.cambus@gmail.com>
2014-01-11 16:07:58 +0100

    Test revert

--- Pong/AppDelegate.h
+++ Pong/AppDelegate.h
@@ -5,7 +5,6 @@
 //  Created by Pablo Cambus on 08/01/2014.
 //  Copyright (c) 2014 Pablo Cambus. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
